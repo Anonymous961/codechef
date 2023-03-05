b09d81d342b316beb685b7e2d363dcefd9e578c2
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-03-05 15:55:00 +0530

    hacckerrank check

--- test.cpp
+++ test.cpp
@@ -1,59 +1,61 @@
 #include <bits/stdc++.h>
+typedef long long int ll;
+typedef long double lld;
+typedef unsigned long long ull;
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define MOD 1000000007
+#define MAX 10000001
+#define MOD1 998244353
+#define INF 1e18
+#define vi vector<int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define ff first
+#define ss second
 using namespace std;
-const int N = 1e5 + 10;
-int a[N];
-void merge(int l, int r, int mid)
-{
-    int l_sz = mid - l + 1;
-    int L[l_sz + 1];
-    int r_sz = r - mid;
-    int R[r_sz + 1];
-    for (int i = 0; i < l_sz; i++)
-    {
-        L[i] = a[i + l];
-    }
-    for (int i = 0; i < r_sz; i++)
-    {
-        R[i] = a[mid + i + 1];
-    }
-    L[l_sz] = R[r_sz] = INT_MAX;
-    int l_i = 0;
-    int r_i = 0;
-    for (int i = l; i <= r; i++)
-    {
-        if (L[l_i] < R[r_i])
-        {
-            a[i] = L[l_i];
-            l_i++;
-        }
-        else
-        {
-            a[i] = R[r_i];
-            r_i++;
-        }
-    }
-}
-void mergeSort(int l, int r)
-{
-    if (l == r)
-        return;
-    int mid = (l + r) / 2;
-    mergeSort(l, mid);
-    mergeSort(mid + 1, r);
-    merge(l, r, mid);
-}
-int main()
+
+void soln()
 {
     int n;
     cin >> n;
+    vi v;
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
+        int x;
+        cin >> x;
+        v.pb(x);
     }
-    mergeSort(0, n - 1);
-    for (int i = 0; i < n; i++)
+    unordered_map<int, int> m;
+    for (auto &i : v)
     {
-        cout << a[i] << " ";
+        m[i]++;
     }
-    cout << endl;
+    int ans, mx_freq = 0;
+    for (auto &i : m)
+    {
+        if (i.second > mx_freq)
+        {
+            ans = i.first;
+            mx_freq = i.second;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    fastio();
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+    soln();
+    // }
+    return 0;
 }
